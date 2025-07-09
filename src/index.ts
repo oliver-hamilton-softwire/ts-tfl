96ec1c60ef6cbc727dd1cc92f5b16ac066795ec8
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-09 10:56:12 +0100

    Wires the postcodes and TfL APIs together

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
+var readlineSync = require('readline-sync');
+
+const APP_KEY = 'e92dcffed66741b09040d3ff8bdc58c7';
+
 const fetchData = async () => {
     try {
+        const postcode = readlineSync.question('Enter postcode: ');
+        // Get details about the postcode
+        const postcodeResponse = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
+        const postcodeJson = await postcodeResponse.json();
+        // Get nearest stops to this postcode
+        const stopsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${postcodeJson.result.latitude}&lon=${postcodeJson.result.longitude}&stopTypes=NaptanPublicBusCoachTram&app_key=${APP_KEY}`);
+        const stopsJson = await stopsResponse.json();
+        // Get the top 2 stops
+        const stops = stopsJson.stopPoints.slice(0, 2);
 
-        const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals?app_key=e92dcffed66741b09040d3ff8bdc58c7");
-        const responseJson = await response.json();
-        console.log(responseJson);
+        for (const stop of stops) {
+            console.log(`Buses at ${stop.commonName}:`);
+            // Get next buses at this stop
+            const nextBusesResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${stop.naptanId}/Arrivals?app_key=${APP_KEY}`);
+            const nextBusesJson = await nextBusesResponse.json();
+            for (const busJson of nextBusesJson) {
+                const busDate: Date = new Date(busJson.expectedArrival);
+                // const busTime: string = `${busDate.getHours()}:${busDate.getMinutes()}:${busDate.getSeconds()}`;
+                console.log(`Line ${busJson.lineId} towards ${busJson.towards} expected at ${busDate.toTimeString().slice(0,8)}`);
+            }
+            console.log();
+        }
     } catch (error) {
         console.error(error);
     } finally {
