4ac6fddca75a4ccf8413eddda5cf98116beb2211
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-09 11:04:25 +0100

    Adds constants

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 var readlineSync = require('readline-sync');
 
 const APP_KEY = 'e92dcffed66741b09040d3ff8bdc58c7';
+const NUMBER_OF_STOPS = 2;
+const BUSES_PER_STOP = 5;
 
 const fetchData = async () => {
     try {
@@ -11,17 +13,17 @@ const fetchData = async () => {
         // Get nearest stops to this postcode
         const stopsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${postcodeJson.result.latitude}&lon=${postcodeJson.result.longitude}&stopTypes=NaptanPublicBusCoachTram&app_key=${APP_KEY}`);
         const stopsJson = await stopsResponse.json();
-        // Get the top 2 stops
-        const stops = stopsJson.stopPoints.slice(0, 2);
+        // Get the top stops
+        const stops = stopsJson.stopPoints.slice(0, NUMBER_OF_STOPS);
 
         for (const stop of stops) {
             console.log(`Buses at ${stop.commonName}:`);
             // Get next buses at this stop
             const nextBusesResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${stop.naptanId}/Arrivals?app_key=${APP_KEY}`);
             const nextBusesJson = await nextBusesResponse.json();
-            nextBusesJson.sort((a: any, b: any) => new Date(a.expectedArrival) - new Date(b.expectedArrival))
+            nextBusesJson.sort((a: any, b: any) => new Date(a.expectedArrival).getTime() - new Date(b.expectedArrival).getTime());
 
-            for (const busJson of nextBusesJson.slice(0, 5)) {
+            for (const busJson of nextBusesJson.slice(0, BUSES_PER_STOP)) {
                 const busDate: Date = new Date(busJson.expectedArrival);
                 // const busTime: string = `${busDate.getHours()}:${busDate.getMinutes()}:${busDate.getSeconds()}`;
                 console.log(`Line ${busJson.lineId} towards ${busJson.towards} expected at ${busDate.toTimeString().slice(0,8)}`);
